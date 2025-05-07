6d38e35cf78fc7cb75708f2029c97108dce1b254
SolDesimone25 <mariasol.desimone@gmail.com>
2025-05-07 17:08:07 -0300

    Pagina Principal, Clase Cliente y Clase ClienteNegocio

--- Grupo 7A/Default.aspx
+++ Grupo 7A/Default.aspx
@@ -1,5 +1,23 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MiMaster.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Grupo_7A.Default" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%--banner--%>
+    <div class="banner">
+        <h1>HOLAAAAAAA</h1>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <div class="mb-3">
+                <label for="txtCodigo" class="form-label">Ingrese el codigo:</label>
+                <asp:TextBox ID="txtCodigo" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <asp:Button ID="btnEnviar" runat="server" CssClass="btn btn-primary" Text="Enviar" />
+        </div>
+        <div class="col-3"></div>
+    </div>
 </asp:Content>
---
+++ negocio/ClienteNegocio.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    internal class ClienteNegocio
+    {
+        public List<Cliente> listar()
+        {
+            List<Cliente> lista = new List<Cliente>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP From Clientes");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Cliente aux = new Cliente();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Documento = (string)datos.Lector["Documento"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Apellido = (string)datos.Lector["Apellido"];
+                    aux.Email = (string)datos.Lector["Email"];
+                    aux.Direccion = (string)datos.Lector["Direccion"];
+                    aux.Ciudad = (string)datos.Lector["Ciudad"];
+                    aux.CodigoPostal = (int)datos.Lector["CP"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void registrar(Cliente nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Insert into Clientes(Documento, Nombre, Apellido, Email, Direccion, Ciudad, CP) values (@Documento, @Nombre, @Apellido, @Direccion, @Email, @Ciudad, @CP)");
+                datos.setearParametro("@Documento", nuevo.Documento);
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Apellido", nuevo.Apellido);
+                datos.setearParametro("@Email", nuevo.Email);
+                datos.setearParametro("@Direccion", nuevo.Direccion);
+                datos.setearParametro("@Ciudad", nuevo.Ciudad);
+                datos.setearParametro("@CP", nuevo.CodigoPostal);
+                datos.ejecutarAccion();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
